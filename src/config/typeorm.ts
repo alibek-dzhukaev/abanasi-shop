ef3552a68bef161f8ca7d4401974bf1db192e69a
Alibek Dzhukaev <alibek.corvus@gmail.com>
2024-03-17 10:02:09 +0300

    feat: migrations

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -2,17 +2,21 @@ import { registerAs } from '@nestjs/config';
 import { config as dotenvConfig } from 'dotenv';
 import { DataSource, DataSourceOptions } from 'typeorm';
 
+console.log('node-env: before', process.env.NODE_ENV);
+
 dotenvConfig({ path: `.env.development` });
+
+console.log('node-env: after', process.env.NODE_ENV);
 
 const config = {
   type: 'postgres',
-  host: `${process.env.POSTGRES_HOST}`,
-  port: parseInt(`${process.env.POSTGRES_PORT}`),
-  username: `${process.env.POSTGRES_USER}`,
-  password: `${process.env.POSTGRES_PASSWORD}`,
-  database: `${process.env.POSTGRES_DB}`,
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
   entities: ['dist/resources/**/*.entity{.ts,.js}'],
-  migrations: ['dist/migrations/*{.ts,.js}'],
+  migrations: ['dist/database/migrations/*{.ts,.js}'],
   synchronize: false,
   migrationsTransactionMode: 'each',
   logger: 'advanced-console',
